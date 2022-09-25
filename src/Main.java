917dd1b5eb3c1170dbefd77a5fc7df1e9d44f36e
Vlad <zitarvlad11@gmail.com>
2022-09-25 19:07:34 +0600

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача 1");
+
+        int apples = 555;
+        System.out.println("Значение переменной apples с типом int равно " + apples);
+        byte bananas = 100;
+        System.out.println("Значение переменной bananas с типом byte равно " + bananas);
+        short watch = 200;
+        System.out.println("Значение переменной watch с типом short равно " + watch);
+        long table = 1000000L;
+        System.out.println("Значение переменной table с типом long равно " + table);
+        float cup = 13f;
+        System.out.println("Значение переменной cup с типом float равно " + cup);
+        double cookies = 14.3;
+        System.out.println("Значение переменной cookies с типом double равно " + cookies);
+    }
+}
